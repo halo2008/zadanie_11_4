f8c6ec7d9e0eec470f26f17b590c40d699803e4b
halo2008 <ksedkowski@gmail.com>
2017-05-14 19:54:47 +0200

    wprowadzony kod

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function Telefon(marka, cena, kolor, ram){
+	this.marka = marka;
+	this.cena = cena;
+	this.kolor = kolor;
+	this.ram = ram;
+
+}
+
+Telefon.prototype.printInfo = function() {
+    console.log("Marka telefonu to " + this.marka + ", kolor telefonu " + this.kolor +  
+                ", a cena telefonu: " +  this.cena + ", pamięć ram " + this.ram + ".");
+}
+
+var Iphone6 = new Telefon('Iphone', '3420', 'zloty', '1GB');
+var SamsungS8 = new Telefon('Samsung Galaxy S8', '3299', 'czarny', '4GB');
+var Xiaoami =  new Telefon('Xiaoami Mi6', '599$', 'Biały', '6GB');
+
+Xiaoami.printInfo();
